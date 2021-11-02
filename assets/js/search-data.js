eebd06bde0ac0a48b9ec18750700ebe29730c792
Karl Kumbier <kkumbier@berkeley.edu>
2021-11-02 14:40:00 -0700

    test

---
+++ assets/js/search-data.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Blueberry orange pie",
+        "excerpt":"Ingredients Filling 20 ounces fresh blueberries (3 1/4 cups; 565g) 8 ounces wild blueberries (2 cups; 225g), fresh or frozen (see note) 1 ounce fresh juice from 1 orange (2 tablespoons; 30g) 1 ounce orange zest 7 ounces sugar (1 cup; 195g) 2 teaspoons vanilla 1/2 teaspoon (2g) Diamond Crystal...","categories": ["desserts"],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/desserts/blueberry-orange-pie.html"
+      },{
+        "title": "Detroit style pizza",
+        "excerpt":"Ingredients Dough: 300g bread flour (10.5 ounces; about 2 generous cups) 5g instant yeast (0.15 ounce; about 1 teaspoon), such as SAF Instant Yeast 9g salt (0.3 ounce; about 1 1/2 teaspoons table salt or 1 tablespoon Diamond Crystal kosher salt) 220g water (7.75 ounces; about 1 cup minus 1...","categories": ["mains"],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/mains/detroit-style-pizza.html"
+      },{
+        "title": "The perfect chocolate chip cookie",
+        "excerpt":"Ingredients 8 ounces unsalted butter 1 standard ice cube 5 ounces bread flour 5 ounces cake flour 3/4 teaspoon baking soda 2 teaspoons Diamond Crystal kosher salt or 1 teaspoon table salt 5 ounces granulated sugar 3 large egg yolks, 1 large egg white 2 teaspoons vanilla extract 5 ounces...","categories": ["desserts"],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/desserts/perfect-chocolate-chip-cookie.html"
+      },{
+        "title": "Pork belly tacos",
+        "excerpt":"Ingredients Pork belly 1 pound pork belly, skin removed 1/2 cup coconut soy sauce juice from 1 lime 1/2 cup coconut vinegar 1/4 cup sriracha 2 tablespoons honey 2 teaspoon salt 1 teaspoon garlic powder 1/4 teaspoon fish saucePineapple salsa 1 cup diced pineapple 1/4 purple cabbage, thinly sliced 1...","categories": ["mains"],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/mains/pork-pelly-tacos.html"
+      },{
+        "title": "Papi chulo sauce",
+        "excerpt":"Ingredients 2 bunches cilantro with stems (washed) 1-2 serrano peppers (with seeds) 1 jalapeño peppers (with seeds) 4 cloves garlic 2 teaspoons rice vinegar 3 tablespoons mustard (see note) juice of 4 limes juice of half lemon juice of 1 orange 1/2 cups olive oil 1 tablespoon sugar 1/2 tablespoon...","categories": ["sauces"],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/sauces/papi-chulo-sauce.html"
+      },{
+        "title": "Pie crust",
+        "excerpt":"Ingredients 8 ounces all-purpose flour, such as King Arthur (1 2/3 cups; 225g), plus more for dusting 1/2 ounce sugar (1 tablespoon; 15g) 1 teaspoon (4g) Diamond Crystal kosher salt; for table salt, use half as much by volume or use the same weight 8 ounces unsalted, American-style butter, straight...","categories": ["doughs"],
+        "tags": [],
+        "url": "http://localhost:4000/recipes/doughs/pie-crust.html"
+      },]
